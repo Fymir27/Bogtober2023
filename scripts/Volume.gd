5d7191d74d01e14adbfd774fc4fded037f8f3aa7
fymir27 <oliver.poeckelhofer@hotmail.com>
2023-10-31 18:07:15 +0100

    fixed volume slider

--- scripts/Volume.gd
+++ scripts/Volume.gd
@@ -1,8 +1,9 @@
 extends HSlider
 
-func _on_value_changed(value:float):
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), -10 + value / 5);
-	if (value == 0):
-		AudioServer.set_bus_mute(AudioServer.get_bus_index("Master"), true);
-	else:
-		AudioServer.set_bus_mute(AudioServer.get_bus_index("Master"), false);
+func _ready():
+    set_volume(value);
+    value_changed.connect(set_volume);
+
+func set_volume(linear: float):
+    var db = linear_to_db(linear);
+    AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), db);
